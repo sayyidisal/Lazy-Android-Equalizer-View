3d8f04372fb8c505123628de03b736b9eaae5e2b
a15dotCom <a15dotCom@gmail.com>
2019-05-21 19:00:09 +0700

    v1.0
    fail lib resources
    fix bug

--- lazylibequalizer/src/main/java/com/ais/lazylibequalizer/LazyEqualizer.java
+++ lazylibequalizer/src/main/java/com/ais/lazylibequalizer/LazyEqualizer.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import com.ais.lazylibequalizer.R;
 
 
 public class LazyEqualizer extends SurfaceView implements SurfaceHolder.Callback {
